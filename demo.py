9d0e8bd142a3c60726725737336e741369c33f46
Tim Horton <hortont424@gmail.com>
2010-04-28 01:22:55 -0400

    LOTS OF FUNCTIONS

--- demo.py
+++ demo.py
@@ -2,26 +2,141 @@ from math import *
 import pyopencl as cl
 import numpy
 
-ctx = cl.create_some_context()
+ctx = cl.Context(dev_type=cl.device_type.GPU)
+print ctx.get_info(cl.context_info.DEVICES)
 queue = cl.CommandQueue(ctx)
 
 pointCount = 100
-n = numpy.float32(1.0)
-l = numpy.float32(0.0)
-m = numpy.float32(0.0)
+n = numpy.int32(1)
+l = numpy.int32(0)
+m = numpy.int32(0)
 
+xyz = 6 * numpy.random.rand(3 * pointCount).astype(numpy.float32) - 3
 output = numpy.zeros(pointCount).astype(numpy.float32)
-dest_buf = cl.Buffer(ctx, cl.mem_flags.WRITE_ONLY, output.nbytes)
+
+mf = cl.mem_flags
+xyz_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=xyz)
+dest_buf = cl.Buffer(ctx, mf.WRITE_ONLY, output.nbytes)
 
 prg = cl.Program(ctx, """
-__kernel void density(__global float ipsi, __global float n, __global float l,
-                      __global float m, __global float * output)
+int L(int a, int b, int x)
+{
+    if(a == 0)
+    {
+        if(b == 0)
+            return 1;
+        else if(b == 1)
+            return 1 - x;
+        else if(b == 2)
+            return 2 - (4 * x) + (x * x);
+        else if(b == 3)
+            return 6 - (18 * x) + (9 * x * x) - (x * x * x);
+    }
+    else if(a == 1)
+    {
+        if(b == 0)
+            return 1;
+        else if(b == 1)
+            return 2 * (2 - x);
+        else if(b == 2)
+            return 3 * (6 - (6 * x) + (x * x));
+        else if(b == 3)
+            return 4 * (24 - (36 * x) + (12 * x * x) - (x * x * x));
+    }
+    else if(a == 2)
+    {
+        if(b == 0)
+            return 2;
+        else if(b == 1)
+            return 6 * (3 - x);
+        else if(b == 2)
+            return 12 * (12 - (8 * x) + (x * x));
+        else if(b == 3)
+            return 20 * (60 - (60 * x) + (15 * x * x) - (x * x * x));
+    }
+    else if(a == 3)
+    {
+        if(b == 0)
+            return 6;
+        else if(b == 1)
+            return 24 * (4 - x);
+        else if(b == 2)
+            return 60 * (20 - (10 * x) + (x * x));
+        else if(b == 3)
+            return 120 * (120 - (90 * x) + (18 * x * x) - (x * x * x));
+    }
+}
+
+float P(int a, int b, float x)
+{
+    if(a == 0)
+    {
+        if(b == 0)
+            return 1.0;
+        else if(b == 1)
+            return x;
+        else if(b == 2)
+            return 0.5 * (-1 + (3.0 * x * x));
+        else if(b == 3)
+            return 0.5 * ((-3.0 * x) + (5.0 * x * x * x));
+    }
+    else if(a == 1)
+    {
+        if(b == 0)
+            return 0.0;
+        else if(b == 1)
+            return -sqrt(1.0 - (x * x));
+        else if(b == 2)
+            return - (3.0 * x) * sqrt(1.0 - (x * x));
+        else if(b == 3)
+            return - (3.0 / 2.0) * sqrt(1.0 - (x * x)) * (-1 + (5 * x * x));
+    }
+    else if(a == 2)
+    {
+        if(b == 0)
+            return 0.0;
+        else if(b == 1)
+            return 0.0;
+        else if(b == 2)
+            return -3.0 * (-1.0 + (x * x));
+        else if(b == 3)
+            return -(15.0 * x) * (-1.0 + (x * x));
+    }
+    else if(a == 3)
+    {
+        if(b == 0)
+            return 0;
+        else if(b == 1)
+            return 0;
+        else if(b == 2)
+            return 0;
+        else if(b == 3)
+            return -15.0 * pow(1.0 - (x * x), (3.0 / 2.0));
+    }
+}
+
+__kernel void density(__global float * xyz, __global float ipsi,
+                      __global int n, __global int l,
+                      __global int m, __global float * output)
 {
     int gid = get_global_id(0);
 
-    float x, y, z, theta, phi, r;
+    float theta, phi, r;
+    float4 pos = {0, 0, 0, 0};
 
-    output[gid] = ipsi;
+    // use vector load?
+    pos.x = xyz[(gid * 3) + 0];
+    pos.y = xyz[(gid * 3) + 1];
+    pos.z = xyz[(gid * 3) + 2];
+
+    // Convert cartesian coordinates to spherical
+    r = length(pos);
+    theta = acos(pos.z / r);
+    phi = atan2(pos.y, pos.x);
+
+
+
+    output[gid] = L(3,2,2);
 }
 """).build()
 
@@ -33,6 +148,6 @@ def independentPsi(n, l):
     return sqrt(rootFirst * rootSecond)
 
 ipsi = numpy.float32(independentPsi(n, l))
-prg.density(queue, [pointCount], ipsi, n, l, m, dest_buf)
+prg.density(queue, [pointCount], xyz_buf, ipsi, n, l, m, dest_buf)
 cl.enqueue_read_buffer(queue, dest_buf, output).wait()
 print output
