e52ec0a94b5169bb6480f5915c515c03f56f88bb
Tim Horton <hortont424@gmail.com>
2010-05-04 14:30:12 -0400

    Comments, a real benchmark system, etc.

--- hydrogen.py
+++ hydrogen.py
@@ -1,66 +1,131 @@
-from math import *
-from time import time
+#!/usr/bin/env python
+
 import pyopencl as cl
 import numpy
+import sys
+
+from math import *
+from time import time
 from PIL import Image
-
-mf = cl.mem_flags
-
-ctx = cl.Context(dev_type=cl.device_type.GPU)
-#print ctx.get_info(cl.context_info.DEVICES)
-queue = cl.CommandQueue(ctx)
-
-kernelFile = open('hydrogen.cl', 'r')
-prg = cl.Program(ctx, kernelFile.read()).build()
-kernelFile.close()
+from optparse import OptionParser
 
 def independentPsi(n, l):
-    a = 1.0
-    rootFirst = (2.0 / (n * a)) ** 3
+    """The first term of psi is constant with respect to the entire image, so
+    it can be computed outside of the inner loop."""
+    rootFirst = (2.0 / n) ** 3
     rootSecond = factorial(n - l - 1.0) / (2.0 * ((n * factorial(n + l)) ** 3))
-
     return sqrt(rootFirst * rootSecond)
 
-def doDensity(ni, li, mi, imageResolution):
+def renderOrbitals((ni, li, mi), imageResolution):
+    """Create various buffers to shuttle data to/from the CPU/GPU, then
+    execute the kernel for each pixel in the input image, and copy the
+    data into a numpy array"""
     res = numpy.int32(imageResolution)
     pointCount = imageResolution ** 2
     n = numpy.int32(ni)
     l = numpy.int32(li)
     m = numpy.int32(mi)
 
+    # Create native buffer and mirror OpenCL buffer
+    mf = cl.mem_flags
     output = numpy.zeros(pointCount).astype(numpy.float32)
-    outputBuffer = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=output)
+    outputBuffer = cl.Buffer(main.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR,
+                             hostbuf=output)
 
+    # Evaluate first term of psi once for entire image
     ipsi = numpy.float32(independentPsi(n, l))
-    prg.density(queue, [pointCount], ipsi, n, l, m, outputBuffer, res)
-    cl.enqueue_read_buffer(queue, outputBuffer, output).wait()
+
+    # Evaluate the rest of psi once for each pixel, copy into output buffer
+    before = time()
+    main.prg.density(main.queue, [pointCount], ipsi, n, l, m, outputBuffer, res)
+    cl.enqueue_read_buffer(main.queue, outputBuffer, output).wait()
+    duration = time() - before
 
     outputBuffer.release()
 
-    return output
+    return (output, duration)
 
 def exportImage(output):
+    """Export a visual representation of the computed density of the
+    wavefunction as a PNG (to /tmp/orbitals.png, for now)"""
+    # Linearly scale image data so that brightest pixel is white
     scaleFactor = 255.0 / max(output)
     for i in range(0, len(output)):
         output[i] *= scaleFactor
 
+    # Output PNG of computed orbital density
     res = int(sqrt(len(output)))
     img = Image.new("L", (res, res))
     img.putdata(output)
     img.show()
     img.save("/tmp/orbitals.png", "PNG")
 
-def densityTiming(res):
-    before = time()
-    output = doDensity(3, 2, 0, res)
-    duration = time() - before
+def benchmark(skip, params=(3,2,0)):
+    """Time computation of the image at various different resolutions; average
+    together 10 trials for each resolution"""
+    for res in range(100,1010,skip):
+        totalDuration = totalSuccessful = 0
 
-    if max(output) > 0.0:
-        return duration
+        for run in range(10):
+            (output, duration) = renderOrbitals(params, res)
+
+            # At some resolutions, on my mobile GPU, I get a totally black image
+            # I have no idea why this happens, but this sits here to discard
+            # those results, since their timing seems to be somewhat inaccurate.
+            if max(output) > 0.0:
+                totalDuration += duration
+                totalSuccessful += 1
+
+        if totalSuccessful > 0:
+            print "{0},{1}".format(res, totalDuration / totalSuccessful)
+
+def main():
+    # Parse commandline arguments
+    parser = OptionParser(usage="%prog [-b/B] [-c] [-p n,l,m]",
+                          version="%prog 0.1")
+    parser.add_option("-b", "--bench", action="store_true", default=False,
+                      dest="benchmark", help="run short benchmark")
+    parser.add_option("-B", "--long-bench", action="store_true",
+                      default=False, dest="longBenchmark",
+                      help="run long benchmark")
+    parser.add_option("-c", "--cpu", action="store_true", default=False,
+                      dest="useCPU", help="run on CPU instead of GPU")
+    parser.add_option("-p", None, default="3,2,0", dest="params",
+                      help="choose parameters to wavefunction")
+    parser.add_option("-r", None, default="400", dest="res",
+                      help="choose resolution of output image")
+    (options, args) = parser.parse_args()
+
+    params = tuple([int(a) for a in options.params.split(",")])
+
+    if options.useCPU:
+        main.ctx = cl.Context(dev_type=cl.device_type.CPU)
     else:
-        return None
+        main.ctx = cl.Context(dev_type=cl.device_type.GPU)
 
-for i in range(100,1000,100):
-    duration = densityTiming(i)
-    if duration:
-        print "{0},{1}".format(i,duration)
+    # Output device(s) being used for computation
+    print "Running on:"
+    for dev in main.ctx.get_info(cl.context_info.DEVICES):
+        print "   ",
+        print dev.get_info(cl.device_info.VENDOR),
+        print dev.get_info(cl.device_info.NAME)
+    print
+
+    # Load and compile the OpenCL kernel
+    main.queue = cl.CommandQueue(main.ctx)
+    kernelFile = open('hydrogen.cl', 'r')
+    main.prg = cl.Program(main.ctx, kernelFile.read()).build()
+    kernelFile.close()
+
+    # Evaluate psi with the given parameters, in the given mode
+    if options.benchmark:
+        benchmark(100, params=params)
+    elif options.longBenchmark:
+        benchmark(10, params=params)
+    else:
+        (output, duration) = renderOrbitals(params, int(options.res))
+        exportImage(output)
+        print duration
+
+if __name__ == "__main__":
+    sys.exit(main())
